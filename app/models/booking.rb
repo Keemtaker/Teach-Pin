0a74cb0f6266aeb2e2865a696149a202b06efaac
Ashlee Stephenson <ashleeste@gmail.com>
2017-11-13 19:13:33 +0100

    created model validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,15 @@ class Booking < ApplicationRecord
   belongs_to :user
 
 
+# needs: status exists - inclusion (pending/confirmed/rejected)
+      #  booking_time - exists, only integer true
+  validate :date_is_in_the_future
+  # validates :status, presence: true, inclusion: {in: %w(pending accepted declined)}
+  validates :booking_time, presence: true
+
+  def date_is_in_the_future
+    if booking_time.present? && booking_time < Time.now
+      errors.add(:booking_time, "Can't be in the past")
+    end
+  end
 end
